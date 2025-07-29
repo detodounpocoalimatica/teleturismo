376892b60635773e9d75e7842657a9894c4a9f88
detodounpocoalimatica <166966246+detodounpocoalimatica@users.noreply.github.com>
2025-07-29 21:14:35 +0200

    Add files via upload

---
+++ config/Nueva carpeta/config.php
@@ -0,0 +1,6 @@
+<?php
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'id6939566_planetariadeteleturismo');
+define('PASSWORD', 'jkmmmm77');
+define('BD', 'id6939566_planetariadeteleturismo');
+?>
